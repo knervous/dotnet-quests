19608ad6bb25dd7dd30efb2adecca6ef24b40796
Vons <26316029+TheVons@users.noreply.github.com>
2024-04-21 16:20:23 -0400

    Update player.cs
    Did not mean to edit the example

--- global/player.cs
+++ global/player.cs
@@ -1,43 +1,5 @@
-class Player : IPlayerEvent
-{
-    public override void Signal(PlayerEvent e)
-    {
-        int value = Convert.ToInt32(e.data);
-
-        if (value ==(int)CRSignals.PlaySoundFromPC)
-        {
-            e.player.PlayMP3("eqtheme.mp3");
-        }
-
-        if (value == (int)CRSignals.CameraShake)
-        {
-            e.player.CameraEffect(2000, 1);
-        }
-
-        if (value == (int)CRSignals.BankerSpawn)
-        {
-            e.player.PlayMP3("banker.mp3");
-        }
-
-        if (value == (int)CRSignals.StripExpBuff)
-        {
-            CRExpBuffs.StripExpBuff(e);
-        }
-    }
-
-    public override void ItemClickClient(PlayerEvent e)
-    {
-        if (e.itemList[0].GetID() == 1800)
-        {
-            if (e.player.GrantFlag(CRUtilityFlags.Banker))
-                e.questManager.WorldWideMessage(questinterface.EQ_Cyan, $"{e.player.GetName()} found a {e.itemList[0].GetItem().Name}!");
-        }
-        else if (e.itemList[0].GetID() == 1801)
-        {
-            if (e.player.GrantFlag(CRUtilityFlags.Merchant))
-                e.questManager.WorldWideMessage(questinterface.EQ_Cyan, $"{e.player.GetName()} found a {e.itemList[0].GetItem().Name}!");
-        }
-
-        // need to implement buffer
+class Player : IPlayerEvent {
+    public void Say(PlayerEvent e) {
+       //e.QuestDebug($"{e.player.GetName()} said: {e.data}");
     }
 }
